c6e568100073ebdb013962ca581d8f54152d3bc0
dipto <diptosaha.ndc50@gmail.com>
2025-03-27 21:47:17 +0600

    Assignment Folder Created

---
+++ BASICS/Assignment On Basics/01printSum.cpp
@@ -0,0 +1,13 @@
+// A program that takes two integers as input and prints their sum.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter the first number :"<<endl;
+    cin>>x;
+    cout<<"Enter the second number :"<<endl;
+    cin>>y;
+    cout<<"The result is : "<<x+y<<endl;
+}
---
+++ BASICS/Assignment On Basics/02areaOfRectangle.cpp
@@ -0,0 +1,13 @@
+// calculate the area of the rectangle.
+#include<bits/stdc++.h>
+    using namespace std;
+int main()
+{
+       int length, breadth,area;
+       cout<<"Enter the value of length  :"<<endl;
+       cin>>length;
+       cout<<"Enter the value of breadth :"<<endl;
+       cin>>breadth;
+       area=length*breadth;
+       cout<<"The value of area is :"<<area<<endl;
+}
